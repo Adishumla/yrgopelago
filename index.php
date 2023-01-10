0351b8ddf1a7b13cf8acccc0bac98c064f4170dc
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-01-10 01:26:06 +0100

    svg on img hover

--- index.php
+++ index.php
@@ -18,7 +18,13 @@ include_once __DIR__ . '/functions.php';
 </head>
 
 <body>
-  <div class="ball"></div>
+  <div class="ball">
+    <div class="ball__inner">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+        <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+      </svg>
+    </div>
+  </div>
   <section class="hero">
     <img src="images/baloon.jpg" alt="">
     <div class="hero-text">
@@ -203,6 +209,23 @@ include_once __DIR__ . '/functions.php';
       });
       button.addEventListener('mouseout', () => {
         ball.style.transform = 'scale(1)'
+      });
+    });
+
+    const images = document.querySelectorAll('.image');
+    const ball__inner = document.querySelector('.ball__inner');
+    images.forEach(image => {
+      image.addEventListener('mouseover', () => {
+        ball.style.transform = 'scale(1.5)'
+        ball.style.transition = 'transform 0.5s ease-in-out';
+        ball.style.transformOrigin = 'center';
+        ball__inner.style.display = 'block';
+        ball__inner.style.mixBlendMode = 'normal';
+
+      });
+      image.addEventListener('mouseout', () => {
+        ball.style.transform = 'scale(1)'
+        ball__inner.style.display = 'none';
       });
     });
   </script>
